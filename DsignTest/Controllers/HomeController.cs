8ebb551c40fdc9c5d12bbc83b5cab2383f67e084
sa-qq <78145233+sa-qq@users.noreply.github.com>
2023-06-14 06:11:49 +0300

    Add project files.

---
+++ DsignTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DsignTest.Controllers
+{
+    public class HomeController : Controller
+    {   
+        public ActionResult LoginPage()
+        {
+            return View("LoginPage");
+        }
+
+        public ActionResult Register()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
